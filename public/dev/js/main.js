590cc7f3d80f5f0862f9d69a260b9fe0eb7b3f2f
Jose San Juan <sanjuanjojo@gmail.com>
2015-03-23 07:56:32 +0800

    Input output part

--- public/dev/js/main.js
+++ public/dev/js/main.js
@@ -1,3 +1,11 @@
 var tempVar = "Hello World!";
 
+
+$(document).ready(function(){
+    $('button').click(function(){
+        tempVar = $('#query').val();
+        $('#test').text(tempVar);
+        console.log('okay');
+    });
+});
 console.log(tempVar);
