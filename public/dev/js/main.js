ace277b8015c701ae9fb67a98780b287d44f1579
Jose San Juan <sanjuanjojo@gmail.com>
2015-03-24 00:43:08 +0800

    Arrange position, Added design to output

--- public/dev/js/main.js
+++ public/dev/js/main.js
@@ -2,19 +2,27 @@ var tempVar;
 var res= [
 		    {
 		        "question": "Is my data being sold to third-parties?",
-		        "res": 1
+		        "res": 1,
+		        "good": "<i class='fa fa-plus-square fa-5x' ",
+		        "bad": "<i class='fa fa-minus-square fa-5x'"
 		    },
 		    {
 		        "question": "Is data transfered by this website encrypted?",
-		        "res": 0
+		        "res": 0,
+		        "good": "<i class='fa fa-plus-square fa-5x' ",
+		        "bad": "<i class='fa fa-minus-square fa-5x' "
 		    },
 		    {
 		        "question": "Does this website store my data even when it's no longer needed?",
-		        "res": 0
+		        "res": 0,
+		        "good": "<i class='fa fa-plus-square fa-5x'",
+		        "bad": "<i class='fa fa-minus-square fa-5x' "
 		    },
 		    {
 		        "question": "Will advertisers have access to my data?",
-		        "res": 1
+		        "res": 1,
+		        "good": "<i class='fa fa-plus-square fa-5x' ",
+		        "bad": "<i class='fa fa-minus-square fa-5x'"
 		    }
 		];
 
@@ -28,9 +36,11 @@ $(document).ready(function(){
         $('#test').text("");
         setTimeout(function(){ 
         display(res);
+        EPPZScrollTo.scrollVerticalToElementById('footer', 20);
         $('#processing-modal').modal('hide') ;
-        }, 3000);
-
+        
+        }, 1000);
+        
     });
 
 });
@@ -52,8 +62,20 @@ function addBinders() {
 };
 
 function display(results){
+	$('#test').append('<div class= "col-md-14"> <h2>' +tempVar+ '</h2></div>');
 	results.forEach(function(rez){
 		console.log(rez.question);
-		$('#test').append('<div class = "col-md-10 col-md-offset-1">' + rez.question + ' '+ rez.res +'</div>');
+		if(rez.res== 0) {
+			$('#test').append('<div class = "row" style="margin-top: 5%"> <div class="col-xs-1 col-xs-offset-3">' +
+			                   rez.bad +'style="color:#F75D59"></i></div> <div class="col-xs-8"> <h4>' + 
+				               rez.question + '</h4>&nbsp;'+ rez.res +'</div></div>');
+		} else {
+			$('#test').append('<div class = "row" style="margin-top: 5%"> <div class="col-xs-1 col-xs-offset-3">' +
+			                   rez.good +'style="color:#3BB9FF"></i></div><div class="col-xs-8"> <h4>' + 
+				               rez.question + '</h4>&nbsp; '+ rez.res +'</div></div>');
+		}
+
+
+
 	});
 }
