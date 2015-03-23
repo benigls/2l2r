2a3aa6d00df01df7438e75f25297c38298d09297
Ben <iglesia.ben@gmail>
2015-03-23 15:35:29 +0800

    Add function to generate XML.

--- submodules/xml.js
+++ submodules/xml.js
@@ -1,2 +1,40 @@
-var Tokenizer = require('sentence-tokenizer');
-var tokenizer = new Tokenizer('Chuck');
+var fs = require('fs'),
+	hypotheses = require('./data/hypotheses');
+
+var XML = function () {
+	this.generateXML = function (text) {
+		var length = hypotheses.length,
+			templateXML = {
+				'@': {
+					'lang': 'EN'
+				},
+				'pair': [
+				]
+			},
+			xml;
+
+		for(var i = 0; i < length; i++) {
+			templateXML.pair[i] = {
+				'@': {
+					'id': i + 1,
+					'entailment': 'UNKNOWN',
+					'task': 'IE'
+				},
+				't': text,
+				'h': hypotheses[i].hypothesis;
+			};
+		}
+
+		xml = js2xmlparser('entailment-corpus', templateXML);
+	};
+
+	var saveXML = function (data) {
+		fs.writeFile("/tmp/data.xml", data, function (err) {
+			if (err) {
+				return console.log(err);
+			}
+		});
+	};
+};
+
+module.exports = new XML();
