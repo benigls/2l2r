18605f81ec95a2b94119becb9e754a5add1bef2c
Jose San Juan <sanjuanjojo@gmail.com>
2015-03-23 13:46:43 +0800

    http:// effect on textbox

--- public/dev/js/main.js
+++ public/dev/js/main.js
@@ -2,12 +2,32 @@ var tempVar;
 
 
 $(document).ready(function(){
+	addBinders();
+    $('.form form query').focus();
+
     $('button').click(function(){
-        setTimeout(function(){ 
         tempVar = $('#query').val();
+        
+        setTimeout(function(){ 
         $('#test').text(tempVar);
         $('#processing-modal').modal('hide') ;
         }, 3000);
 
     });
+
 });
+
+var addBinders = function addBinders() {
+	$('.form form').find('input').on('focus', function(){
+		var content = $(this).val();
+		if(content === "") {
+			$(this).val("http://");
+		}
+	});
+
+	$('.form form').find('input').on('blur', function(){
+		if($(this).val() === "http://") {
+			$(this).val("");
+		}
+	});
+};
